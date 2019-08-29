380fadfb620d6e3a2d5282a17360948bb004e31f
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-30 00:09:42 +0100

    create the App Component

--- marketplace/src/app/app.component.ts
+++ marketplace/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // tslint:disable-next-line: component-selector
+  selector: 'pm-root',
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <div>My first app component</div>
+    </div>
+  `
 })
+
 export class AppComponent {
-  title = 'marketplace';
+  pageTitle = 'Shola Product Management';
 }
