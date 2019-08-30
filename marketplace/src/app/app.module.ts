c6d4f0da3183e184c7773da5d7fb87430b81009f
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-30 12:46:41 +0100

    using a component as a directive

--- marketplace/src/app/app.module.ts
+++ marketplace/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
