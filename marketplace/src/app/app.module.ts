97f5670ebcb3dfc5fe26fb09434d3b9443ce49d8
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-04 13:59:13 +0100

    subscribing to an observable

--- marketplace/src/app/app.module.ts
+++ marketplace/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { StarComponent } from './shared/star.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
